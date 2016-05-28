c93d372e833318e2850f0183540470621b487fcd
tom@cgfindies.co.uk <tom@cgfindies.co.uk>
2016-05-28 18:21:28 +0100

    Random messages now use doThis() abstract

--- AndroidRedisMessenger/app/src/main/java/uk/co/cgfindies/androidredismessenger/async/RandomMessageRunnable.java
+++ AndroidRedisMessenger/app/src/main/java/uk/co/cgfindies/androidredismessenger/async/RandomMessageRunnable.java
@@ -2,21 +2,18 @@ package uk.co.cgfindies.androidredismessenger.async;
 
 import android.content.Context;
 
-import org.droidparts.util.L;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.exceptions.JedisConnectionException;
 import uk.co.cgfindies.androidredismessenger.R;
 import uk.co.cgfindies.androidredismessenger.storage.JedisProvider;
 
 /**
  * Provides a random message once every 20 seconds on average.
  */
-public class RandomMessageRunnable extends RepeatRunnable
+public class RandomMessageRunnable extends RepeatRunnable implements JedisProvider.DoThisInterface
 {
 
     private static RandomMessageRunnable instance = null;
@@ -27,6 +24,8 @@ public class RandomMessageRunnable extends RepeatRunnable
     private int arrayHitMax;
 
     private String[] values;
+    private long messageTime;
+    private String messageContent;
 
     /**
      * Set up the runner to get a random string every 20 seconds on average.
@@ -62,51 +61,29 @@ public class RandomMessageRunnable extends RepeatRunnable
     @Override
     public void run()
     {
-        long messageTime = System.currentTimeMillis();
+        messageTime = System.currentTimeMillis();
         Random random = new Random(messageTime);
         int hit = random.nextInt(maxRandom);
 
         if (hit <= hitMax)
         {
-            Jedis jedis = null;
-
-            try
-            {
-                jedis = JedisProvider.getInstance().getJedisInstance();
-
-                int arrayIndex = Math.round(hit / arrayHitMax);
-                try
-                {
-                    String username = jedis.srandmember("usernames");
-
-                    Map<String, String> message = new HashMap<>();
-                    message.put("message", values[arrayIndex]);
-                    message.put("timestamp", Long.toString(messageTime));
-                    message.put("username", username);
-
-                    jedis.hmset("messages:" + Long.toString(messageTime), message);
-                    jedis.rpush("messageKeys", Long.toString(messageTime));
-                }
-                catch (JedisConnectionException e)
-                {
-                    L.w(e);
-                }
-            }
-            catch (ArrayIndexOutOfBoundsException e)
-            {
-                L.w("The random hit could not calculate an index for the string. Number generated was "
-                        + Integer.toString(hit)
-                        + " with an arrayHitMax of "
-                        + Integer.toString(arrayHitMax));
-            }
-            finally
-            {
-                if (jedis != null) {
-                    jedis.close();
-                }
-            }
+            messageContent = values[Math.round(hit / arrayHitMax)];
+            JedisProvider.doThis(this);
         }
 
         super.run();
+    }
+
+    @Override
+    public void doThis(Jedis jedis) {
+        String username = jedis.srandmember("usernames");
+
+        Map<String, String> message = new HashMap<>();
+        message.put("message", messageContent);
+        message.put("timestamp", Long.toString(messageTime));
+        message.put("username", username);
+
+        jedis.hmset("messages:" + Long.toString(messageTime), message);
+        jedis.rpush("messageKeys", Long.toString(messageTime));
     }
 }
